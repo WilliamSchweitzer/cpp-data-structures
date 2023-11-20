396aacf0ee590dfd4be2837419ecfb598771f225
WilliamSchweitzer <weschweitzer@outlook.com>
2023-11-20 09:18:10 -0500

    Add initial list.cpp

---
+++ abstract-data-types/list.cpp
@@ -0,0 +1,31 @@
+/* List Definitions:
+ * 1. A list contains elements a1...an
+ * 2. A list with zero elements will be a null list
+ * 3. For any non null list, i will be the position of an element in the list
+ * 4. ai+1 will be the successor of ai and ai-1 will be the predeccessor of ai
+ * 5. Any elements are allowed to be stored in a list
+ * 6. Index starts at 0
+ *
+ * Operations:
+ * print_list - output the list
+ * make_null - make the list have zero elements
+ * find - returns the position of the first occurrence of a key
+ * insert/delete - insert or delete an element at a given position i in the list
+ * find_kth(arg k) - retun the element in some position k (specified in the argument)
+ *
+ * Examples:
+ * Given list 7, 4, 3, 8
+ * find(4) would return 2
+ * insert(x, 2) would make the list into 7, 4, x, 3, 8 (go to position 2 and insert after)
+ * delete(2) would modify the list resulting in 7, x, 3, 8
+ *
+ * Note: More operations can be added and the exact implementation of certain functions is up to the programmer. The the element is not in the list when calling find(arg) what is returned for example?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, world!";
+	return 0;
+}
